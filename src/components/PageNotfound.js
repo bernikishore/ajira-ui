2710fa6c7dd65bcf870cfcb60cf230f4413e9ae0
Kishore N <arunkishore1991@gmail.com>
2020-09-06 17:23:58 +0530

    account page added

--- src/components/PageNotfound.js
+++ src/components/PageNotfound.js
@@ -12,7 +12,7 @@ export default class PageNotFound extends Component {
             <div className="container-md">
                 <div className="not-found-wrapper">
                     <div className="innner-wrapper">
-                        <h3>404 page not found</h3>
+                        <h3>404<span>page not found</span></h3>
                         <p>We are sorry but the page you are looking for does not exist.</p>
                     </div>
                 </div>
